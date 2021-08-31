6d6b057f279d371eba5876c2b972596862f9d576
JiaSheng <projiasheng98@gmail.com>
2021-09-01 00:35:52 +0100

    experiment on threshholding and dilate

--- main.py
+++ main.py
@@ -14,14 +14,25 @@ Img = Img[250:400, 175:545]  # cropping img to relevant parts
 
 Img = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)  # convert img to grayscale
 
-thresh = 200  # experiment for suitable threshold
-im_bw = cv2.threshold(Img, thresh, 255, cv2.THRESH_BINARY)[1]
+#thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+#dilation
+def dilate(image):
+    kernel = numpy.ones((5,5),numpy.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
 
-text = tess.image_to_string(Img, config='--psm 4 stdout -c tessedit_char_whitelist=-.?xmvV0123456789')  #pass img to tess with whitelist char
+# im_dilated = dilate(Img)
+# im_bw = thresholding(Img)
+
+custom_config = '--psm 4 --oem 3 outputbase digits'
+# custom_config = '--psm 4 --oem 3 stdout -c tessedit_char_whitelist=-.?xmvV0123456789 outputbase digits'
+text = tess.image_to_string(Img, config=custom_config)  #pass img to tess with whitelist char
 # text = tess.image_to_string(im_bw, config='--psm 4 stdout -c tessedit_char_whitelist=-.?mV0123456789')  #pass img to tess with whitelist char
 
 print(text)  # currently grey works better than BW
 
 cv2.imshow('Gray image', Img)
+# cv2.imshow('dilate image', im_dilated)
 cv2.imshow('BW image', im_bw)
 cv2.waitKey(0)
