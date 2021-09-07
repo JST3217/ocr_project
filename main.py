4564e7696d2335550f03fa42e8419f8cfedf24c3
Jia-Automata <jia@automata.tech>
2021-09-07 01:23:25 +0100

    made function to get all image path from dir. ocr results to raw .txt, post-processed results to processed .txt

--- main.py
+++ main.py
@@ -3,70 +3,68 @@ from PIL import Image
 import numpy as np
 import matplotlib
 import pytesseract as tess
+import os
 
-# function to build: for each item in Reports, save the name, then construct img path
+in_filename = 'MU_Main_Tab_0_Analog.png'  # only look at this img file
+folderPath = 'Reports'
+myRobots = os.listdir(folderPath)  # get robot directory name
 
-# img_path = 'Reports/' + 'Report_210810_082307' + '/MU_Main_Tab_0_Analog.png'
-# img_path = 'Reports/' + 'Report_210810_143330' + '/MU_Main_Tab_0_Analog.png'
+robot_counter = 0
+img_path = []
 
-# img_path = 'Reports/EV4.15U01-2128-002/' + 'Report_210712_141129' + '/MU_Main_Tab_0_Analog.png'
-# img_path = 'Reports/EV4.15U01-2128-002/' + 'Report_210712_164050' + '/MU_Main_Tab_0_Analog.png'
-# img_path = 'Reports/EV4.15U01-2128-002/' + 'Report_210713_081243' + '/MU_Main_Tab_0_Analog.png'
-img_path = 'Reports/EV4.15U01-2128-002/' + 'Report_210713_123928' + '/MU_Main_Tab_0_Analog.png'
+for reports in myRobots:
+    reports = os.listdir(folderPath + '/' + reports)
+    for report in reports:
+        img_path.append(folderPath + '/' + myRobots[robot_counter] + '/' + report + '/' + in_filename)
+    robot_counter = robot_counter + 1
 
-# img_path = 'Reports/EV4.15U01-2129-002/' + 'Report_210720_081826' + '/MU_Main_Tab_0_Analog.png'
-# img_path = 'Reports/EV4.15U01-2129-002/' + 'Report_210720_085549' + '/MU_Main_Tab_0_Analog.png'
-# img_path = 'Reports/EV4.15U01-2129-002/' + 'Report_210720_090241' + '/MU_Main_Tab_0_Analog.png'
-# img_path = 'Reports/EV4.15U01-2129-002/' + 'Report_210720_090920' + '/MU_Main_Tab_0_Analog.png'
+for img in img_path:
+    Img = cv2.imread(img)
+    # cv2.imshow('Img',Img)
+    # cv2.waitKey(0)
 
-Img = cv2.imread(img_path)
+    Img = Img[250:400, 175:545]  # cropping img to relevant parts
 
-cv2.imshow('Img',Img)
-cv2.waitKey(0)
+    # cv2.imshow('Img',Img)
+    # cv2.waitKey(0)
 
-Img = Img[250:400, 175:545]  # cropping img to relevant parts
+    Img = cv2.cvtColor(Img, cv2.COLOR_BGR2RGB)  # convert img to RGB
+    white_px = (255, 255, 255)
 
-cv2.imshow('Img',Img)
-cv2.waitKey(0)
+    x1 = 190
+    x2 = 325
+    Img[0:150, x1:x1+40] = white_px  # remove useless stuff between readings
+    Img[0:150, x2:x2+40] = white_px  # remove useless stuff between readings
+    Img = Image.fromarray(Img)  # build img from array
 
-Img = cv2.cvtColor(Img, cv2.COLOR_BGR2RGB)  # convert img to RGB
-white_px = (255, 255, 255)
-black_px = (0, 0, 0)
+    # Img = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)  # convert img to grayscale
 
-x1 = 190
-x2 = 325
-Img[0:150, x1:x1+40] = white_px  # remove useless stuff between readings
-Img[0:150, x2:x2+40] = white_px  # remove useless stuff between readings
-Img = Image.fromarray(Img)  # build img from array
+    # thresholding
+    def thresholding(image):
+        return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    # dilation
+    def dilate(image):
+        kernel = np.ones((5, 5), np.uint8)
+        return cv2.dilate(image, kernel, iterations=1)
+    # im_dilated = dilate(Img)
+    # im_bw = thresholding(Img)
 
-# Img = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)  # convert img to grayscale
+    # custom_config = '--psm 4 --oem 3 outputbase digits'
+    custom_config = '--psm 4 --oem 3 stdout -c tessedit_char_whitelist=-.?xmv0123456789 outputbase digits'
+    text = tess.image_to_string(Img, config=custom_config)  # pass img to tess with whitelist char
+    # text = tess.image_to_string(im_bw, config='--psm 4 stdout -c tessedit_char_whitelist=-.?mV0123456789')  #pass img to tess with whitelist char
 
-# thresholding
-def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-# dilation
-def dilate(image):
-    kernel = numpy.ones((5, 5), numpy.uint8)
-    return cv2.dilate(image, kernel, iterations=1)
-# im_dilated = dilate(Img)
-# im_bw = thresholding(Img)
+    out_filename = img[8:26] + '_' + img[27:47]
+    out_filepath = 'Outputs/Raw/'+out_filename+'.txt'
+    print(text,  file=open(out_filepath, 'w'))
 
-# custom_config = '--psm 4 --oem 3 outputbase digits'
-custom_config = '--psm 4 --oem 3 stdout -c tessedit_char_whitelist=-.?xmv0123456789 outputbase digits'
-text = tess.image_to_string(Img, config=custom_config)  # pass img to tess with whitelist char
-# text = tess.image_to_string(im_bw, config='--psm 4 stdout -c tessedit_char_whitelist=-.?mV0123456789')  #pass img to tess with whitelist char
+    with open(out_filepath) as infile, open('Outputs/Processed/'+out_filename+'.txt', 'w') as outfile:
+        for line in infile:
+            if not line.strip(): continue  # skip the empty line
+            outfile.write(line)  # non-empty line. Write it to output
 
-print(text,  file=open('log.txt', 'w'))
+    f = open('Outputs/Processed/'+out_filename+'.txt', 'r')
+    file_contents = f.read()
+    print (file_contents)
+    f.close()
 
-with open('log.txt') as infile, open('output.txt', 'w') as outfile:
-    for line in infile:
-        if not line.strip(): continue  # skip the empty line
-        outfile.write(line)  # non-empty line. Write it to output
-
-f = open('output.txt', 'r')
-file_contents = f.read()
-print (file_contents)
-f.close()
-
-Img.show()
-# cv2.waitKey(0)
